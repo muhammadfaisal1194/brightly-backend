3d090096ee57e86a8c4d37a196b92fc64ba9bac1
muhammadfaisal1194 <muhammadfaisal1194@gmail.com>
2024-07-08 15:46:59 +0500

    port reverted

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post("/subscribe", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
