c7f0e2f80e9439f3d98d1c743f4a620aa2ebe809
muhammadfaisal1194 <muhammadfaisal1194@gmail.com>
2024-07-08 14:58:44 +0500

    api added

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send({
+    message: "subscription server!!",
+  });
+});
+
+app.post("/subscribe", async (req, res) => {
+  const email = req.body.email;
+
+  const data = {
+    data: {
+      type: "profile",
+      attributes: {
+        email: email,
+      },
+    },
+  };
+
+  try {
+    const response = await axios.post(
+      "https://a.klaviyo.com/api/profiles/",
+      data,
+      {
+        headers: {
+          accept: "application/json",
+          revision: "2024-06-15",
+          "content-type": "application/json",
+          Authorization: `Klaviyo-API-Key ${process.env.API_KEY}`,
+        },
+      }
+    );
+
+    res.status(200).json({ success: true, data: response.data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred",
+      error: error.message,
+    });
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
